ebe1c9ecb4c51de94ec5595b20b4c1b564b16864
gsilber <silber@udel.edu>
2024-05-29 15:50:00 -0400

    docs update

--- Webz-core/src/WebzRouter.ts
+++ Webz-core/src/WebzRouter.ts
@@ -16,10 +16,30 @@ export interface Route {
 }
 declare const URLHREF: string;
 
+/**
+ * @description: A router
+ * @export
+ * @class WebzRouter
+ * @group Routing
+ */
 export class WebzRouter {
     private baseRoute: string = URLHREF || "";
     private currentComponent: WebzComponent | null = null;
     private selectedPage: number = 0;
+    /**
+     * Creates an instance of WebzRouter.
+     * @param {WebzComponent} container - the container component
+     * @param {Route[]} routes - the routes
+     * @param {string} id - the id to place the routed content
+     * @memberof WebzRouter
+     * @group Routing
+     * @example
+     * const router = new WebzRouter(container, [
+     *  { path: "/", component: new Home() },
+     *  { path: "/about", component: new About() },
+     *  { path: "/contact", component: new Contact() },
+     *  ], "content");
+     */
     constructor(
         private container: WebzComponent,
         private routes: Route[],
@@ -28,9 +48,28 @@ export class WebzRouter {
         this.route(window.location.pathname.replace(this.baseRoute, ""));
     }
 
+    /**
+     * @description: Gets the selected route
+     * @memberof WebzRouter
+     * @group Routing
+     * @returns {number} - the selected route
+     */
     selectedRoute(): number {
         return this.selectedPage;
     }
+    /**
+     * @description: Routes to a path
+     * @memberof WebzRouter
+     * @group Routing
+     * @param {string} path - the path to route to
+     * @example
+     * const router = new WebzRouter(container, [
+     *  { path: "/", component: new Home() },
+     *  { path: "/about", component: new About() },
+     *  { path: "/contact", component: new Contact() },
+     *  ], "content");
+     * router.route("/about");
+     */
     route(path: string) {
         const route = this.routes.find((r) => r.path === path);
         if (route) {
